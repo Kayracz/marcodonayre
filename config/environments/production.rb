2b914a60cb3d151a1a7e7fa2c582228eea80249e
Kayra Czerniewicz <kayracz@gmail.com>
2019-03-29 10:52:41 -0400

    herokuerrors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -71,7 +72,6 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
